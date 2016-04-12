c313395efe9c1332511d8e4a358cca83c8dadd30
Mengjie <mengjie.zhao@outlook.com>
2016-04-12 21:37:52 +0200

    SQL and ERD updated

--- submitONE/reviseDDL.sql
+++ submitONE/reviseDDL.sql
@@ -1,44 +1,67 @@
 CREATE TABLE Author
     (   Author_ID           INTEGER,
-        Author_Name         CHAR(16),
-        Author_Legal_Name   CHAR(16),
-        Author_Last_Name    CHAR(16),
-        Pseudonym           CHAR(16),
-        Birth_Place         CHAR(16),
-        Birth_Date          DATE,
-        Death_Date          DATE,
-        Email_Address       CHAR (64),
-        Author_Image        CHAR(128),   /*URL?*/
+        Author_Name         VARCHAR,
+        Author_Legal_Name   VARCHAR,
+        Author_Last_Name    VARCHAR,
+        Pseudonym           VARCHAR,
+        Birth_Place         VARCHAR,
+        Birth_Date          VARCHAR,
+        Death_Date          VARCHAR,
+        Email_Address       VARCHAR,   
         AuthorLanguage_ID   INTEGER, 
+        noteID              VARCHAR,
+        webPageID           VARCHAR,
 
-        primary key(Author_ID),
-        FOREIGN KEY (Author_Language_ID)  REFERENCES Language(Language_ID);
-        FOREIGN KEY (Author_Image)        REFERENCES Webpages(URL));
+        primary key (Author_ID),
+        FOREIGN KEY (Author_Language_ID)  REFERENCES Language(Language_ID),
+        FOREIGN KEY (webPageID)           REFERENCES  WEBPAGE(webPageID),
+        FOREIGN KEY (noteID)              REFERENCES Note(noteID));
+
+
+CREATE TABLE Author_lang
+    (   Author_ID           INTEGER,
+        Language_ID         INTEGER,
+        Language_name       VARCHAR
+        
+        PRIMARY KEY (Author_ID, Language_ID),
+        FOREIGN KEY (Author_ID)         REFERENCES Author(Author_ID),
+        FOREIGN KEY (Language_ID)       REFERENCES Language(Language_ID)
+        FOREIGN KEY (Language_name)     REFERENCES Language(Name));
+
+
 
 CREATE TABLE WRITES
     (   pa_ID               INTEGER,
-        Author_ID           INTEGER NOT NULL,
-        Publication_ID      INTEGER NOT NULL,
+        Author_ID           INTEGER,
+        Publication_ID      INTEGER,
         
-        PRIMARY KEY (pa_ID),
+        PRIMARY KEY (Author_ID, Publication_ID),
         FOREIGN KEY (Author_ID)         REFERENCES Author(Author_ID),
         FOREIGN KEY (Publication_ID)    REFERENCES Publication(Publication_ID));
 
+
+
 CREATE TABLE Language
     (   Language_ID         INTEGER,
-        Name                CHAR(16),
-        Language_Code       CHAR(8),
-        Language_Script     CHAR(16),
+        Name                VARCHAR,
+        Language_Code       VARCHAR,
+        Language_Script     VARCHAR,
 
         PRIMARY KEY (Language_ID));
 
-CREATE TABLE Note
-    (   Note_ID     INTEGER,
-        Note        CHAR(256) );
 
-CREATE TABLE Webpage
-    (   Webpage_ID          INTEGER,
-        URL                 CHAR(128),
+
+CREATE TABLE Note
+    (   noteID      INTEGER,
+        NOTE       VARCHAR 
+
+        PRIMARY KEY noteID);
+
+
+
+CREATE TABLE WEBPAGE
+    (   webPageID           INTEGER,
+        URL                 VARCHAR,
         Author_ID           INTEGER,
         Pub_Serie_ID        INTEGER,
         Title_ID            INTEGER,
@@ -47,110 +70,54 @@ CREATE TABLE Webpage
         Award_cateID        INTEGER,
         Award_type_ID       INTEGER,
         
-        PRIMARY KEY (Webpage_ID),
+        PRIMARY KEY (webPageID),
         FOREIGN KEY (Author_ID)     REFERENCES Author(Author_ID))
         FOREIGN KEY (Pub_Serie_ID)  REFERENCES Publication_Serie(Pub_Serie_ID)),
         FOREIGN KEY (Publisher_ID)  REFERENCES Publisher(Publisher_ID)),
         FOREIGN KEY (Title_ID)      REFERENCES Title(Title_ID)),
         FOREIGN KEY (Tit_sris_ID)   REFERENCES Title_series(Tit_sris_ID)),
-        FOREIGN KEY (Award_cateID ) REFERENCES Award_categories(Award_cateID )),
+        FOREIGN KEY (Award_cateID ) REFERENCES Award_categories(Award_cateID)),
         FOREIGN KEY (Award_typeID)  REFERENCES Award_types(Award_typeID));
 
 
 
 CREATE TABLE Title(
     Title_ID                INTEGER,
-    Title                   CHAR(128),
-    Title_synopsis          INTEGER,       
-    Series_ID               INTEGER,
-    Series_num              INTEGER,
-    Story_len               CHAR(32),
-    Title_type              CHAR(64),
-    Title_graph             CHAR(8),
-
+    Title                   VARCHAR,      
+    Series_num              VARCHAR,
+    Story_len               VARCHAR,
+    Title_type              VARCHAR,
+    Title_graph             VARCHAR,
+    
+    Title_synopsis          INTEGER, 
     Parent              INTEGER,
-    Pubc_ID             INTEGER NOT NULL,
-    Review_ID           INTEGER,
-    Language_ID         INTEGER NOT NULL,
-    Tag_ID              INTEGER,
-    Note_ID             INTEGER,      
-    Title_tsltorID      INTEGER,    
+    Language_ID         INTEGER NOT NULL,/*BUT THE DATA HAS NONE!!*/
+    noteID              INTEGER,      
+    Title_tsltorID      INTEGER, 
+    Series_ID           INTEGER,  
+    webPageID           VARCHAR, 
 
     PRIMARY KEY (Title_ID),
     FOREIGN KEY (Parent)            REFERENCES Title(Title_ID),
-    FOREIGN KEY (pubc_ID)           REFERENCES Publication_content(pubc_ID),
-    FOREIGN KEY (Review_ID)         REFERENCES Reviews(Review_ID),
     FOREIGN KEY (Language_ID)       REFERENCES Language(Language_ID),
-    FOREIGN KEY (Tag_ID)            REFERENCES Tags(Tag_ID),
-    FOREIGN KEY (Note_ID)           REFERENCES Note(Note_ID),
-    FOREIGN KEY (Title_synopsis)    REFERENCES Note(Note_ID),
-    FOREIGN KEY (Title_tsltorID)    REFERENCES Title_translator(Title_tsltorID));
+    FOREIGN KEY (noteID)            REFERENCES Note(noteID),
+    FOREIGN KEY (Title_synopsis)    REFERENCES Note(noteID),
+    FOREIGN KEY (Series_ID)         REFERENCES Title_series(Tit_sris_ID),
+   /* FOREIGN KEY (Title_tsltorID)    REFERENCES Title_translator(Title_tsltorID)*/
+                                                             );
 
-CREATE TABLE Title_translator(
-    Title_tsltorID      INTEGER,
-    Title_tsltorName    CHAR(64),
+CREATE TABLE Title_series(
+    Tit_sris_ID        INTEGER,
+    Sris_title         VARCHAR,
+    Sris_parent        VARCHAR,
+    Sris_noteID        INTEGER,
 
-    PRIMARY KEY (Title_tsltorID));   
-
-
-
-
-CREATE TABLE Publication_content (
-    pubc_ID          INTEGER,
-    
-    Title_ID         INTEGER,
-    Publication_ID   INTEGER,
-
-    PRIMARY KEY (pubc_ID),
-    FOREIGN KEY (Title_ID)         REFERENCES Title(Title_ID),
-    FOREIGN KEY (Publication_ID)   REFERENCES Publication(Publication_ID));
-
-/* I build the publication table for convenience when connecting title and
- publication so this table is incomplete and could be incorrect. Feel free
- to modify and delete anything */
- 
-CREATE TABLE Publication(
-    Publication_ID     INTEGER,
-    Public_title       CHAR(64),
-    Public_date        CHAR(64),
-    Public_pages       CHAR(32),
-    Pack_type          CHAR(32),
-    Public_type        CHAR(64),
-    ISBN               CHAR(128),
-    Price              CHAR(32),
-    Pub_Serie_ID       INTEGER,
-    Public_srisNum     INTEGER,
-
-    PublisherID         INTEGER    NOT NULL,
-    Public_image        CHAR(256),  /*THE URL*/
-    Note_ID             INTEGER,
-
-    PRIMARY KEY (ISBN),
-    FOREIGN KEY (PublisherID)       REFERENCES Publisher(PublisherID),
-    FOREIGN KEY (Public_image)      REFERENCES Webpage(URL),
-    FOREIGN KEY (Note_ID)           REFERENCES Note(Note_ID),
-    FOREIGN KEY (Pub_Serie_ID)      REFERENCES Publication_series(Pub_Serie_ID));
-
-CREATE TABLE Publication_series
-    (    Pub_Serie_ID    INTEGER,
-         Pub_Serie_Name  CHAR(30),
-         Publication_ID  INTEGER  NOT NULL, 
-         Note_ID         INTEGER,
-
-        PRIMARY KEY (Pub_Serie_ID),
-        FOREIGN KEY (Note_ID)         REFERENCES Note(Note_ID),         
-        FOREIGN KEY (Publication_ID)  REFERENCES Publication (Publication_ID));
-
-CREATE TABLE Publisher
-    (   PublisherID     INTEGER,
-        Publisher_Name  CHAR(30),
-        Note_ID         INTEGER,
-        PRIMARY KEY (PublisherID),
-        FOREIGN KEY (Note_ID)        REFERENCES Note(Note_ID));
+    PRIMARY KEY (Tit_sris_ID),
+    FOREIGN KEY (Sris_parent)   REFERENCES Title_series(Sris_title),
+    FOREIGN KEY (Sris_noteID)   REFERENCES Note(noteID));
 
 CREATE TABLE Reviews(
-    Rev_ID         INTEGER,
-    
+    Rev_ID         INTEGER, 
     Title_ID       INTEGER,
     Review_ID      INTEGER,
 
@@ -158,82 +125,137 @@ CREATE TABLE Reviews(
     FOREIGN KEY(Title_ID)    REFERENCES Title(Title_ID),
     FOREIGN KEY(Review_ID)   REFERENCES Title(Title_ID));
 
+/*CREATE TABLE Title_translator(
+    Title_tsltorID      INTEGER,
+    Title_tsltorName    VARCHAR,
 
-CREATE TABLE Title_series(
-    Tit_sris_ID        INTEGER,
-    Sris_title         CHAR(128),
+    PRIMARY KEY (Title_tsltorID));   */
 
-    Sris_parent        CHAR(128),
-    Sris_note_ID       INTEGER,
-
-    PRIMARY KEY (Tit_sris_ID),
-    FOREIGN KEY (Sris_parent)   REFERENCES Title_series(Sris_title),
-    FOREIGN KEY (Sris_note_ID)  REFERENCES Note(Note_ID));
 
 CREATE TABLE Title_tags(
-    Tagmap_ID      INTEGER,
-    
+    Tagmap_ID      INTEGER, 
     Tag_ID         INTEGER,
     Title_ID       INTEGER,
 
-    PRIMARY KEY (Tagmap_ID),
+    PRIMARY KEY (Tag_ID, Title_ID),
     FOREIGN KEY (Tag_ID)      REFERENCES Tags(Tag_ID),
     FOREIGN KEY (Title_ID)    REFERENCES Title(Title_ID));
 
 
 CREATE TABLE Tags(
     Tag_ID          INTEGER,
-    Tag_name        CHAR(64),
+    Tag_name        VARCHAR,
 
     PRIMARY KEY (Tag_ID));
 
+
 CREATE TABLE Title_awards(
     taw_ID          INTEGER,
-
     Award_ID        INTEGER,
     Title_ID        INTEGER,
 
-    PRIMARY KEY (taw_ID),
+    PRIMARY KEY (Award_ID, Title_ID),
     FOREIGN KEY (Award_ID)    REFERENCES Awards(Award_ID),
     FOREIGN KEY (Title_ID)    REFERENCES Title(Title_ID));
 
 CREATE TABLE Awards(
     Award_ID         INTEGER,
-    Award_title      CHAR(256),
-    Award_date       CHAR(64),
+    Award_title      VARCHAR,
+    Award_date       VARCHAR,
     
-    Award_typeCode    CHAR(16) NOT NULL,
+    Award_typeCode    VARCHAR,
     Award_typeID      INTEGER  NOT NULL,
-    Award_cateID      INTEGER  NOT NULL,
-    Note_ID           INTEGER,
+    Award_cateID      INTEGER,
+    noteID            INTEGER,
 
     PRIMARY KEY (Award_ID),
     FOREIGN KEY (Award_typeID)     REFERENCES Award_types(Award_typeID),
-    FOREIGN KEY (Award_cateID)     REFERENCES Award_categories(Award_cateID),
-    FOREIGN KEY (Note_ID)          REFERENCES Note(Note_ID) );
+  /*FOREIGN KEY (Award_cateID)     REFERENCES Award_categories(Award_cateID),*/
+    FOREIGN KEY (noteID)           REFERENCES Note(noteID));
 
-CREATE TABLE Award_categories(
-    Award_cateID         INTEGER,
-    Award_cateName       CHAR(128),
-    Award_cate_typeID    INTEGER,
-    Award_cateOrd        INTEGER,
-
-    Note_ID              INTEGER,
-
-    PRIMARY KEY (Award_cateID),
-    FOREIGN KEY (Note_ID)        REFERENCES Note(Note_ID));
 
 CREATE TABLE Award_types(
     Award_typeID         INTEGER,
-    Award_typeCode       CHAR(64),
-    Award_typeName       CHAR(128),
-    Award_by             CHAR(64),
-    Award_for            CHAR(64),
-    short_name           CHAR(64),
-    poll                 CHAR(8),
-    Awd_type_Non_g       CHAR(64),
-
-    Note_ID              INTEGER,
+    Award_typeCode       VARCHAR,
+    Award_typeName       VARCHAR,
+    Award_by             VARCHAR,
+    Award_for            VARCHAR,
+    short_name           VARCHAR,
+    poll                 VARCHAR,
+    Awd_type_Non_g       VARCHAR,
+ 
+    noteID               INTEGER,
 
     PRIMARY KEY (Award_typeID),
-    FOREIGN KEY (Note_ID) REFERENCES Note(Note_ID));
+    FOREIGN KEY (noteID) REFERENCES Note(noteID));
+
+
+CREATE TABLE Award_categories(
+    Award_cateID         INTEGER,
+    Award_cateName       VARCHAR,
+    Award_cate_typeID    INTEGER,
+    Award_cateOrd        INTEGER,
+
+    noteID               INTEGER,
+    Award_typeID         INTEGER NOT NULL,
+
+    PRIMARY KEY (Award_cateID, Award_typeID),
+    FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID) ON DELETE CASCADE,
+    FOREIGN KEY (noteID)       REFERENCES Note(noteID));
+
+
+CREATE TABLE Publication(
+    Publication_ID     INTEGER,
+    Public_title       VARCHAR,
+    Public_date        VARCHAR,
+    Public_pages       VARCHAR,
+    Pack_type          VARCHAR,
+    Public_type        VARCHAR,
+    ISBN               VARCHAR,
+    Price              VARCHAR,
+    Pub_Serie_ID       INTEGER,
+    Public_srisNum     INTEGER,
+    PublisherID        INTEGER,
+    Public_image       VARCHAR,  
+    noteID             INTEGER,
+    webPageID       INTEGER,
+
+    PRIMARY KEY (ISBN),
+    FOREIGN KEY (PublisherID)       REFERENCES Publisher(PublisherID),
+    FOREIGN KEY (noteID)            REFERENCES Note(noteID),
+    FOREIGN KEY (Pub_Serie_ID)      REFERENCES Publication_series(Pub_Serie_ID)
+    FOREIGN KEY (webPageID)         REFERENCES WEBPAGE(webPageID));
+
+
+CREATE TABLE Publication_content (
+    pubc_ID          INTEGER,  
+    Title_ID         INTEGER,
+    Publication_ID   INTEGER,
+
+    PRIMARY KEY (Title_ID, Publication_ID),
+    FOREIGN KEY (Title_ID)         REFERENCES Title(Title_ID),
+    FOREIGN KEY (Publication_ID)   REFERENCES Publication(Publication_ID));
+
+
+CREATE TABLE Publication_series
+    (    Pub_Serie_ID    INTEGER,
+         Pub_Serie_Name  VARCHAR,
+         noteID          INTEGER,
+
+
+        PRIMARY KEY (Pub_Serie_ID),
+        FOREIGN KEY (noteID)       REFERENCES Note(noteID));
+
+CREATE TABLE Publisher
+    (   PublisherID     INTEGER,
+        Publisher_Name  VARCHAR,
+        noteID          INTEGER,
+        webPageID       INTEGER,
+
+        PRIMARY KEY (PublisherID),
+        FOREIGN KEY (noteID)       REFERENCES Note(noteID)
+        FOREIGN KEY (webPageID)    REFERENCES WEBPAGE(webPageID));
+
+
+
+
