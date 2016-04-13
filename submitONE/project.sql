f1c5fe58a522b2db36b8ddac7b10dff3efb7384c
Mengjie <mengjie.zhao@outlook.com>
2016-04-13 17:18:35 +0200

    sequencial problem fixed

--- submitONE/project.sql
+++ submitONE/project.sql
@@ -30,21 +30,18 @@ CREATE TABLE Title_series(
 
 CREATE TABLE Title(
     Title_ID                INTEGER,
-    Title                   VARCHAR(128),      
-    Series_num              VARCHAR(128),
+    Title                   VARCHAR(128), 
+    transla_lang            VARCHAR(64),
+    name_of_transla         VARCHAR(128),  
+    Title_synopsis          INTEGER,
+    noteID                  INTEGER, 
+    Tit_sris_ID             INTEGER,
+    Series_num              VARCHAR(32),
     Story_len               VARCHAR(64),
     Title_type              VARCHAR(64),
+    Parent                  INTEGER,
+    Language_ID             INTEGER,
     Title_graph             VARCHAR(16),
-    translator_lang         VARCHAR(64),
-    translator_name         VARCHAR(128),
-    
-    Title_synopsis      INTEGER, 
-    Parent              INTEGER,
-    Language_ID         INTEGER,/*should be not null BUT THE DATA HAS NONE!!*/
-    noteID              INTEGER,      
-    Title_tsltorID      INTEGER, 
-    Tit_sris_ID         INTEGER,  
-    webPageID           INTEGER, 
 
     PRIMARY KEY (Title_ID),
     FOREIGN KEY (Parent)            REFERENCES Title(Title_ID),
@@ -52,6 +49,8 @@ CREATE TABLE Title(
     FOREIGN KEY (noteID)            REFERENCES Note(noteID),
     FOREIGN KEY (Title_synopsis)    REFERENCES Note(noteID),
     FOREIGN KEY (Tit_sris_ID)       REFERENCES Title_series(Tit_sris_ID));
+
+
     
 CREATE TABLE Title_tags(
     Tagmap_ID      INTEGER, 
@@ -76,14 +75,13 @@ CREATE TABLE Award_types(
     Award_typeID         INTEGER,
     Award_typeCode       VARCHAR(128),
     Award_typeName       VARCHAR(128),
+    noteID               INTEGER,   
     Award_by             VARCHAR(128),
     Award_for            VARCHAR(128),
     short_name           VARCHAR(128),
     poll                 VARCHAR(16),
     Awd_type_Non_g       VARCHAR(64),
  
-    noteID               INTEGER,
-
     PRIMARY KEY (Award_typeID),
     FOREIGN KEY (noteID) REFERENCES Note(noteID));
     
@@ -92,19 +90,17 @@ CREATE TABLE Award_categories(
     Award_cateName       VARCHAR(128),
     Award_cate_typeID    INTEGER,
     Award_cateOrd        INTEGER,
-
     noteID               INTEGER,
-    Award_typeID         INTEGER NOT NULL,
+ /*    Award_typeID         INTEGER NOT NULL,*/
 
     PRIMARY KEY (Award_cateID),
-    FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID) ON DELETE CASCADE,
+/*    FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID) ON DELETE CASCADE,*/
     FOREIGN KEY (noteID)       REFERENCES Note(noteID));
     
 CREATE TABLE Awards(
     Award_ID         INTEGER,
     Award_title      VARCHAR(128),
     Award_date       DATE,
-    
     Award_typeCode    VARCHAR(128),
     Award_typeID      INTEGER  NOT NULL,
     Award_cateID      INTEGER,
@@ -112,7 +108,7 @@ CREATE TABLE Awards(
 
     PRIMARY KEY (Award_ID),
     FOREIGN KEY (Award_typeID)     REFERENCES Award_types(Award_typeID),
-  /*FOREIGN KEY (Award_cateID)     REFERENCES Award_categories(Award_cateID),*/
+    FOREIGN KEY (Award_cateID)     REFERENCES Award_categories(Award_cateID),
     FOREIGN KEY (noteID)           REFERENCES Note(noteID));
     
 CREATE TABLE Title_awards(
@@ -132,20 +128,6 @@ CREATE TABLE Publication_series
          
         PRIMARY KEY (Pub_Serie_ID),
         FOREIGN KEY (noteID)       REFERENCES Note(noteID));
-        
-        
-CREATE TABLE WEBPAGE
-    (   webPageID           INTEGER,
-        URL                 VARCHAR(256),
-        Author_ID           INTEGER,
-        Pub_Serie_ID        INTEGER,
-        Title_ID            INTEGER,
-        Publisher_ID        INTEGER,
-        Tit_sris_ID         INTEGER, 
-        Award_cateID        INTEGER,
-        Award_typeID        INTEGER,
-        
-        PRIMARY KEY (webPageID)       );
         
 
 CREATE TABLE Publisher
@@ -168,14 +150,14 @@ CREATE TABLE Author
         Birth_Place         VARCHAR(128),
         Birth_Date          DATE,
         Death_Date          DATE,
-        Email_Address       VARCHAR(128),   
+        Email_Address       VARCHAR(128), 
+        Author_image        VARCHAR(256)  
         Language_ID         INTEGER, 
         noteID              INTEGER,
-        webPageID           INTEGER,
+
 
         primary key (Author_ID),
         FOREIGN KEY (Language_ID)         REFERENCES  Language(Language_ID),
-        FOREIGN KEY (webPageID)           REFERENCES  WEBPAGE(webPageID),
         FOREIGN KEY (noteID)              REFERENCES  Note(noteID));
 
 
@@ -193,37 +175,34 @@ CREATE TABLE Publication(
     Publication_ID     INTEGER,
     Public_title       VARCHAR(128),
     Public_date        DATE,
+    Publisher_ID       INTEGER,
     Public_pages       VARCHAR(128),
     Pack_type          VARCHAR(128),
     Public_type        VARCHAR(128),
-    ISBN               VARCHAR(128),
-    Pub_Serie_ID       INTEGER,
-    Public_srisNum     INTEGER,
-    Publisher_ID       INTEGER,
-    Public_image       VARCHAR(128),  
-    noteID             INTEGER,
-    webPageID          INTEGER,
+    ISBN               VARCHAR(128),   
+    front_cover_img    VARCHAR(256),   
     currency_sign      VARCHAR(16),
     currency_amout     VARCHAR(64),
-
+    noteID             INTEGER,    
+    Pub_Serie_ID       INTEGER,
+    Public_srisNum     INTEGER,
 
     PRIMARY KEY (Publication_ID),
     FOREIGN KEY (Publisher_ID)      REFERENCES Publisher(Publisher_ID),
     FOREIGN KEY (noteID)            REFERENCES Note(noteID),
-    FOREIGN KEY (Pub_Serie_ID)      REFERENCES Publication_series(Pub_Serie_ID),
-    FOREIGN KEY (webPageID)         REFERENCES WEBPAGE(webPageID));
+    FOREIGN KEY (Pub_Serie_ID)      REFERENCES Publication_series(Pub_Serie_ID));
 
-CREATE TABLE WRITES
+CREATE TABLE Publication_authors
     (   pa_ID               INTEGER,
+        Publication_ID      INTEGER,        
         Author_ID           INTEGER,
-        Publication_ID      INTEGER,
-        
+      
         PRIMARY KEY (Author_ID, Publication_ID),
         FOREIGN KEY (Author_ID)         REFERENCES Author(Author_ID),
         FOREIGN KEY (Publication_ID)    REFERENCES Publication(Publication_ID));
 
 
-CREATE TABLE Publication_content (
+CREATE TABLE Publication_content(
     pubc_ID          INTEGER,  
     Title_ID         INTEGER,
     Publication_ID   INTEGER,
@@ -232,14 +211,35 @@ CREATE TABLE Publication_content (
     FOREIGN KEY (Title_ID)         REFERENCES Title(Title_ID),
     FOREIGN KEY (Publication_ID)   REFERENCES Publication(Publication_ID));
 
-/*webpage*/
-/*ALTER TABLE WEBPAGE ADD CONSTRAINT  pk_webpage     PRIMARY KEY (webPageID);*/
+
+CREATE TABLE WEBPAGE
+    (   webPageID           INTEGER,
+        Author_ID           INTEGER,
+        Publisher_ID        INTEGER,
+        URL                 VARCHAR(256),
+        Pub_Serie_ID        INTEGER,
+        Title_ID            INTEGER,
+        Award_typeID        INTEGER,
+        Tit_sris_ID         INTEGER, 
+        Award_cateID        INTEGER,
+        
+        
+        PRIMARY KEY (webPageID),
+        FOREIGN KEY (Author_ID)    REFERENCES Author(Author_ID),
+        FOREIGN KEY (Pub_Serie_ID) REFERENCES Publication_series(Pub_Serie_ID),
+        FOREIGN KEY (Publisher_ID) REFERENCES Publisher(Publisher_ID),
+        FOREIGN KEY (Title_ID)     REFERENCES Title(Title_ID),
+        FOREIGN KEY (Tit_sris_ID)  REFERENCES Title_series(Tit_sris_ID),
+        FOREIGN KEY (Award_cateID) REFERENCES Award_categories(Award_cateID),
+        FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID));
+
+/*
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage1     FOREIGN KEY (Author_ID)    REFERENCES Author(Author_ID);
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage2     FOREIGN KEY (Pub_Serie_ID) REFERENCES Publication_series(Pub_Serie_ID);
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage3     FOREIGN KEY (Publisher_ID) REFERENCES Publisher(Publisher_ID);
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage4     FOREIGN KEY (Title_ID)     REFERENCES Title(Title_ID);
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage5     FOREIGN KEY (Tit_sris_ID)  REFERENCES Title_series(Tit_sris_ID);
 ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage6     FOREIGN KEY (Award_cateID) REFERENCES Award_categories(Award_cateID);
-ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage7     FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID);
+ALTER TABLE WEBPAGE ADD CONSTRAINT  fk_webpage7     FOREIGN KEY (Award_typeID) REFERENCES Award_types(Award_typeID);*/
 
       
